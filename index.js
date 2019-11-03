5c2930ff45eef364413a24d088d746d80c82e567
VadimKalayda <vadosgta1@gmail.com>
2019-11-03 17:37:25 +0200

    some changes

--- index.js
+++ index.js
@@ -23,21 +23,38 @@ function openMenuIconFunc() {
 
 // RANDOM DATES
 
-let randomDatesArr = [],
+let randomDatesArr = new Array(10),
+	indexes = [...Array(10).keys()],
 	date1 = new Date(1970, 1, 1);
-	date2 = new Date();
+date2 = new Date();
 
-function randomDate(from, to) {
+function shuffle(arr) {
+	for(let i = arr.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+	}
+	return arr;
+}
+
+indexes = shuffle(indexes);
+console.log('indexes', indexes);
+
+function randomDate(arr, from, to) {
 	for(let i = 0; i < 10; i++) {
 		let randomDateNumber = new Date(from.getTime() + Math.random() * (to.getTime() - from.getTime()));
-		randomDatesArr.push(randomDateNumber) * Math.floor();
+		arr[[indexes[i]]] = randomDateNumber;
 	}
-	localStorage.setItem('Dates', JSON.stringify(randomDatesArr));
+	localStorage.setItem('Dates', JSON.stringify(arr));
+	console.log('Dates', randomDatesArr);
 }
+
+randomDate(randomDatesArr, date1, date2);
+sortDates(randomDatesArr);
 
 function sortDates(arr) {
 	arr.sort(function(a,b) {
 		return a - b;
 	});
 	localStorage.setItem('Sorted Dates', JSON.stringify(randomDatesArr));
+	console.log('Sorted Dates', randomDatesArr);
 }
